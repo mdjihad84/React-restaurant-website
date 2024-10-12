2d0ad3e365a1cc63b8ed6b60ac13f1889cbd75c8
mdjihad84 <jahidulislamjihad@outlook.com>
2024-10-12 06:37:33 +0600

    img setup complete

--- src/Component/HeroSection.jsx
+++ src/Component/HeroSection.jsx
@@ -3,8 +3,8 @@ const Hero = () => {
         <div className="block">
             <div>
                 <img 
-                    className="w-full h-auto sm:h-[300px] md:h-[500px] lg:h-[700px] xl:h-[804px]"
-                    src="/Images/Hero.jpg" alt="Hero"/>
+                    className="w-full h-auto sm:h-[300px] md:mt-[80px] md:h-[500px] lg:h-[700px] xl:h-[804px]"
+                    src="/Images/Hero.jpg" alt="Hero-img"/>
             </div>
             <div>
                 <img className="mx-auto mt-2" src="/Images/Food.jpg" alt="Food"/>
--- src/Component/Testimonials.jsx
+++ src/Component/Testimonials.jsx
@@ -15,7 +15,7 @@ const Testimonials = () => {
                 <i className="fa-regular fa-star"></i>
             </div>
             <div className="mt-2">
-                <img src="/public/Images/Group.png" className="mx-auto max-w-[400px] md:max-w-[600px]" alt="Group-img" />
+                <img src="/Images/Group.png" className="mx-auto max-w-[400px] md:max-w-[600px]" alt="Group-img" />
                 <p className="mx-auto w-full max-w-[920px] text-center mt-4 px-4">
                     <i className="fa-solid fa-arrow-left text-blue-500 text-xl mr-2"></i>
                     Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humor<i className="fa-solid fa-arrow-right text-blue-500 text-xl ml-2"></i> and the like). It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
